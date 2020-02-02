3d230828ddf7196c799b1f325de365a42a35df75
oWX748827 <ostanin.oleg@huawei.com>
2020-02-02 14:41:54 +0300

    add specialties

---
+++ moto-workshop-data/src/main/java/example/olegostanin/motoworkshop/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package example.olegostanin.motoworkshop.services;
+
+import example.olegostanin.motoworkshop.model.Speciality;
+
+/**
+ * Created by jt on 7/31/18.
+ */
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
